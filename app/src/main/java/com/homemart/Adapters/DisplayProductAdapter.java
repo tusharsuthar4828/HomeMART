637759c2fbdd21b907351cd8ed1d61af2b8b0bfa
tusharsuthar4828 <tusharsuthar4828@gmail.com>
2020-03-20 18:22:20 +0530

    ui changes

--- app/src/main/java/com/homemart/Adapters/DisplayProductAdapter.java
+++ app/src/main/java/com/homemart/Adapters/DisplayProductAdapter.java
@@ -1,14 +1,22 @@
 package com.homemart.Adapters;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CalendarView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.fragment.app.FragmentManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.homemart.Fragments.DisplayEachProduct;
+import com.homemart.Fragments.Home;
 import com.homemart.R;
 import com.homemart.models.Product;
 
@@ -36,6 +44,17 @@ public class DisplayProductAdapter extends RecyclerView.Adapter<DisplayProductAd
     public void onBindViewHolder(@NonNull DisplayProuctViewHolder holder, int position) {
         holder.mNameTextView.setText(mProductList.get(position).getName()+"");
         holder.mPriceTextView.setText(mProductList.get(position).getPrice()+"");
+        holder.mProductCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(holder.itemView.getContext(), "", Toast.LENGTH_SHORT).show();
+                Context context = holder.itemView.getContext();
+                FragmentManager fragmentManager = ((AppCompatActivity)context).getSupportFragmentManager();
+                fragmentManager.beginTransaction().replace(R.id.fragment_conatiner, new DisplayEachProduct(), "DisplayEachProduct").commit();
+                
+            }
+        });
     }
 
     @Override
@@ -43,16 +62,20 @@ public class DisplayProductAdapter extends RecyclerView.Adapter<DisplayProductAd
         return mProductList.size();
     }
 
-    class DisplayProuctViewHolder extends RecyclerView.ViewHolder{
+    class DisplayProuctViewHolder extends RecyclerView.ViewHolder {
 
         TextView mNameTextView;
         TextView mPriceTextView;
+        CardView mProductCard;
 
         public DisplayProuctViewHolder(@NonNull View itemView) {
             super(itemView);
 
             mNameTextView = itemView.findViewById(R.id.productname);
             mPriceTextView = itemView.findViewById(R.id.productprice);
+            mProductCard = itemView.findViewById(R.id.product_cardview);
+
         }
+
     }
 }
